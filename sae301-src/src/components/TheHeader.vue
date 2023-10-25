8c1de0d3b5c1b6caf29f6dd7d2d17120ae179cd6
Hammachi-Dalil <116789242+Hammachi-Dalil@users.noreply.github.com>
2023-10-25 14:05:29 +0200

    bouton header fixé a droite

--- sae301-src/src/components/TheHeader.vue
+++ sae301-src/src/components/TheHeader.vue
@@ -1,11 +1,25 @@
 <script setup>
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
     <div style="background-color: #FFEFCF;">
-
+        
+            <nav style="text-align: right; margin-right: 10vh;">
+                <RouterLink to="/" button class="nav-btn">Accueil</RouterLink>
+                <RouterLink to="/LunetteView" button class="nav-btn">Lunettes</RouterLink>
+                <RouterLink to="/ConnexionView" button class="nav-btn">Connexion</RouterLink>       
+            </nav>
     
-    <p>menu du site</p>
-    <p>si tu lis sa c'est que le footer s'affiche correctement</p>
 </div>
 </template>
+
+<style scoped>
+.nav-btn {
+  margin-right: 7vh;
+  text-decoration: none;
+  color: black;
+  font-size: xx-large;
+  font-family: 'GFS Didot', serif;
+}
+</style>
